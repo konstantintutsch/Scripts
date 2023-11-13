87521fb3937b46d3640759d586db1fe06ee23f79
Konstantin Tutsch <mail@konstantintutsch.com>
2023-11-13 16:23:09 +0100

    Add FirewallD configuration script

---
+++ firewalld.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# /etc/firewalld/firewalld.conf: IndividiualCalls=yes
+# firewall-cmd --reload won't work otherwise
+
+printf "set default zone to drop:\n"
+firewall-cmd --set-default-zone=drop
+printf "\n"
+
+
+open_ports="$(firewall-cmd --list-ports)"
+for open_port in $open_ports
+do
+	printf "remove %s: " "$open_port"
+	firewall-cmd --remove-port="$open_port"
+done
+printf "\n"
+
+
+# Ports
+ports[0]="22/tcp"     # SSH !! IMPORTANT
+
+for port in "${ports[@]}"
+do
+	printf "add %s: " "$port"
+	firewall-cmd --add-port="$port"
+done
+
+
+printf "\nmaking changes permanent: "
+firewall-cmd --runtime-to-permanent
+
+printf "reloading: "
+firewall-cmd --reload
