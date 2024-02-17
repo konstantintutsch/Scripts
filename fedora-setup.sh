e5572e275791f153d79fcf7e272500ef962fd511
Konstantin Tutsch <mail@konstantintutsch.com>
2024-02-17 20:24:45 +0100

    Update application list

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 SYSTEM_REPOSITORIES=("kallepm/tuxedo-keyboard" "kallepm/tuxedo-control-center")
-SYSTEM_PACKAGES=("adw-gtk3-theme" "fuse-sshfs" "gnome-console" "gnome-tweaks" "neovim" "jekyll" "source-foundry-hack-fonts" "syncthing" "tlp" "toot" "tuxedo-control-center" "tuxedo-keyboard" "ufraw" "dropbox" "nautilus-dropbox")
-USER_PACKAGES=("com.belmoussaoui.Decoder" "com.belmoussaoui.Obfuscate" "com.github.ADBeveridge.Raider" "com.github.Eloston.UngoogledChromium" "com.github.huluti.Curtail" "com.github.tchx84.Flatseal" "com.spotify.Client" "de.haeckerfelix.Fragments" "de.schmidhuberj.tubefeeder" "dev.geopjr.Collision" "dev.geopjr.Tuba" "fr.romainvigier.MetadataCleaner" "io.github.celluloid_player.Celluloid" "io.github.mrvladus.List" "io.gitlab.librewolf-community" "io.gitlab.news_flash.NewsFlash" "net.ankiweb.Anki" "org.darktable.Darktable" "org.gaphor.Gaphor" "org.gimp.GIMP" "org.gnome.Geary" "org.gnome.Maps" "org.gnome.Snapshot" "org.gnome.World.PikaBackup" "org.gnome.World.Secrets" "org.gtk.Gtk3theme.adw-gtk3" "org.inkscape.Inkscape" "org.localsend.localsend_app" "org.nickvision.money" "org.pitivi.Pitivi")
+SYSTEM_PACKAGES=("adw-gtk3-theme" "fuse-sshfs" "gnome-console" "gnome-tweaks" "neovim" "jekyll" "source-foundry-hack-fonts" "tlp" "toot" "tuxedo-control-center" "tuxedo-keyboard" "ufraw" "zoxide")
+USER_PACKAGES=("com.belmoussaoui.Obfuscate" "com.cburch.Logisim" "com.github.Eloston.UngoogledChromium" "com.github.huluti.Curtail" "com.github.tchx84.Flatseal" "com.spotify.Client" "com.valvesoftware.Steam" "de.schmidhuberj.tubefeeder" "dev.alextren.Spot" "dev.geopjr.Tuba" "fr.romainvigier.MetadataCleaner" "hu.kramo.Cartridges" "io.github.celluloid_player.Celluloid" "io.gitlab.librewolf-community" "io.gitlab.news_flash.NewsFlash" "me.kozec.syncthingtk" "net.ankiweb.Anki" "org.darktable.Darktable" "org.gimp.GIMP" "org.gnome.Maps" "org.gnome.World.PikaBackup" "org.gnome.World.Secrets" "org.gtk.Gtk3theme.adw-gtk3" "org.inkscape.Inkscape" "org.localsend.localsend_app" "org.mozilla.Thunderbird" "org.nickvision.money" "org.pitivi.Pitivi")
 
 # Add system repositories
 for REPOSITORY in "${SYSTEM_REPOSITORIES[@]}"
