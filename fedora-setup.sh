58e74d254dc3888c6304266aa7cea27223d403cb
Konstantin Tutsch <mail@konstantintutsch.com>
2024-06-12 16:15:13 +0200

    Add package list generation instructions

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/bash
 
 SYSTEM_REPOSITORIES=("kallepm/tuxedo-keyboard" "kallepm/tuxedo-control-center")
-SYSTEM_PACKAGES=("adw-gtk3-theme" "fuse-sshfs" "gnome-console" "gnome-tweaks" "neovim" "jekyll" "source-foundry-hack-fonts" "tlp" "toot" "tuxedo-control-center" "tuxedo-keyboard" "ufraw" "zoxide")
-USER_PACKAGES=("com.belmoussaoui.Obfuscate" "com.cburch.Logisim" "com.github.Eloston.UngoogledChromium" "com.github.huluti.Curtail" "com.github.tchx84.Flatseal" "com.spotify.Client" "com.valvesoftware.Steam" "de.schmidhuberj.tubefeeder" "dev.alextren.Spot" "dev.geopjr.Tuba" "fr.romainvigier.MetadataCleaner" "hu.kramo.Cartridges" "io.github.celluloid_player.Celluloid" "io.gitlab.librewolf-community" "io.gitlab.news_flash.NewsFlash" "me.kozec.syncthingtk" "net.ankiweb.Anki" "org.darktable.Darktable" "org.gimp.GIMP" "org.gnome.Maps" "org.gnome.World.PikaBackup" "org.gnome.World.Secrets" "org.gtk.Gtk3theme.adw-gtk3" "org.inkscape.Inkscape" "org.localsend.localsend_app" "org.mozilla.Thunderbird" "org.nickvision.money" "org.pitivi.Pitivi")
+
+# RPMs
+# Generate: for package in $(dnf repoquery --userinstalled --queryformat "%{name}"); do printf "\"%s\" " "${package}"; done; printf "\n"
+#     + manual sort
+SYSTEM_PACKAGES=("adw-gtk3-theme" "arm-image-installer" "fuse-sshfs" "gnome-tweaks" "gstreamer1-plugin-openh264" "gstreamer1-plugins-bad-free-extras" "gstreamer1-plugins-bad-free-fluidsynth" "gstreamer1-plugins-bad-free-opencv" "gstreamer1-plugins-bad-free-wildmidi" "gstreamer1-plugins-bad-free-zbar" "gstreamer1-plugins-good-extras" "gstreamer1-plugins-good-qt6" "htop" "mozilla-fira-mono-fonts" "neofetch" "neovim" "nodejs" "nodesource-release" "openssh-askpass" "perl-Image-ExifTool" "python3-pip" "source-foundry-hack-fonts" "sqlitebrowser" "tlp" "toot" "tuxedo-control-center" "tuxedo-keyboard" "ufraw" "yt-dlp" "zoxide")
+
+# Flatpaks
+# Generate: for package in $(flatpak list --user --app --columns=application); do printf "\"%s\" " "${package}"; done; printf "\n"
+USER_PACKAGES=("com.belmoussaoui.Obfuscate" "com.cburch.Logisim" "com.discordapp.Discord" "com.github.huluti.Curtail" "com.github.tchx84.Flatseal" "com.google.Chrome" "com.mardojai.ForgeSparks" "com.spotify.Client" "com.valvesoftware.Steam" "de.schmidhuberj.Flare" "de.zwarf.picplanner" "dev.geopjr.Tuba" "fr.romainvigier.MetadataCleaner" "io.github.Foldex.AdwSteamGtk" "io.github.celluloid_player.Celluloid" "io.github.david_swift.Flashcards" "io.github.fizzyizzy05.binary" "io.gitlab.librewolf-community" "io.gitlab.liferooter.TextPieces" "io.gitlab.news_flash.NewsFlash" "me.kozec.syncthingtk" "net.ankiweb.Anki" "org.darktable.Darktable" "org.gimp.GIMP" "org.gnome.Builder" "org.gnome.Maps" "org.gnome.Weather" "org.gnome.World.PikaBackup" "org.gnome.World.Secrets" "org.gnome.gitlab.cheywood.Buffer" "org.gnome.gitlab.somas.Apostrophe" "org.inkscape.Inkscape" "org.kde.kdenlive" "org.localsend.localsend_app" "org.mozilla.Thunderbird" "org.nickvision.money" "org.tenacityaudio.Tenacity" "page.kramo.Cartridges" "so.libdb.dissent")
 
 # Add system repositories
 for REPOSITORY in "${SYSTEM_REPOSITORIES[@]}"
