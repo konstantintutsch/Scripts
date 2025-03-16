46c18d111a95def4ffcfbc5ad3ce38403616fb5a
Konstantin Tutsch <mail@konstantintutsch.com>
2025-03-16 16:05:55 +0100

    refactor: streamline font installation
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

--- apple-emoji.sh
+++ apple-emoji.sh
@@ -4,9 +4,29 @@
 SOURCE="${HOME}/Downloads/AppleColorEmoji.ttf"
 curl -L --verbose https://github.com/samuelngs/apple-emoji-linux/releases/latest/download/AppleColorEmoji.ttf -o "${SOURCE}"
 
-# Install font on system
-sudo -u root cp --verbose "${SOURCE}" "/usr/share/fonts"
-sudo -u root fc-cache --verbose --force
+# Disable alternative emoji fonts
+DISABLE="<selectfont>
+    <rejectfont>
+        <glob>*NotoColorEmoji.ttf*</glob>
+        <glob>/usr/share/fonts/noto-emoji/*</glob>
+        <glob>/usr/share/fonts/google-noto-color-emoji-fonts/*</glob>
+    </rejectfont>
+</selectfont>"
+
+# Install font for user
+ROOT="${HOME}/.local/share/fonts"
+FONT="${HOME}/.config/fontconfig"
+
+# Install
+mkdir --verbose --parent "${ROOT}"
+cp --verbose "${SOURCE}" "${ROOT}"
+
+# Disable alternatives
+mkdir --verbose --parent "${FONT}"
+echo "${DISABLE}" > "${FONT}/fonts.conf"
+
+# Update fonts
+fc-cache --verbose --force
 
 # Install font to sandboxed apps (Flatpak)
 for APP in "${HOME}/.var/app/"*
@@ -19,10 +39,11 @@ do
     mkdir --verbose --parent "${FLATPAK}"
     cp --verbose "${SOURCE}" "${FLATPAK}"
 
-    # Disable alternative emoji fonts
+    # Disable alternatives
     mkdir --verbose --parent "${FONT}"
-    echo "<selectfont><rejectfont><glob>*NotoColorEmoji.ttf*</glob><glob>/usr/share/fonts/noto-emoji/*</glob><glob>/usr/share/fonts/google-noto-color-emoji-fonts/*</glob></rejectfont></selectfont>" > "${FONT}/fonts.conf"
+    echo "${DISABLE}" > "${FONT}/fonts.conf"
 
+    # Update
     flatpak run --command=fc-cache "${ID}" --verbose --force
 done
 
