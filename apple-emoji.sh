4106d50dfedb71de972aaa2621b0f3dbb9393357
Konstantin Tutsch <mail@konstantintutsch.com>
2025-03-16 13:28:48 +0100

    feat: add Apple Emoji font installer
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

---
+++ apple-emoji.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Download source font
+SOURCE="${HOME}/Downloads/AppleColorEmoji.ttf"
+curl -L --verbose https://github.com/samuelngs/apple-emoji-linux/releases/latest/download/AppleColorEmoji.ttf -o "${SOURCE}"
+
+# Install font on system
+sudo -u root cp --verbose "${SOURCE}" "/usr/share/fonts"
+sudo -u root fc-cache --verbose --force
+
+# Install font to sandboxed apps (Flatpak)
+for APP in "${HOME}/.var/app/"*
+do
+    ID="$(basename ${APP})"
+    FLATPAK="${APP}/data/fonts"
+
+    mkdir --verbose --parent "${FLATPAK}"
+    cp --verbose "${SOURCE}" "${FLATPAK}"
+
+    flatpak run --command=fc-cache "${ID}" --verbose --force
+done
+
+# Remove source font
+rm --verbose --interactive=never "${SOURCE}"
