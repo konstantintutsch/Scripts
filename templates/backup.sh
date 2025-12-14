2320af50c7fb5a5b400d7e1d0f02ccfd47ebd9e2
Konstantin Tutsch <mail@konstantintutsch.com>
2025-12-14 14:33:01 +0100

    fix: inconsistent backup crash
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

--- templates/backup.sh
+++ templates/backup.sh
@@ -1,57 +1,95 @@
 #!/bin/bash
 
+LOGFILE="backup.log"
+
+# Clean up logfile
+if [ -f "$LOGFILE" ]
+then
+	rm --interactive=never "$LOGFILE"
+fi
+
+# Run screen if not already inside
+if [ -z "$STY" ]
+then
+	exec screen -L -Logfile "$LOGFILE" -S backup /bin/bash "$0"
+fi
+
 # Exit on error
 set -eE
-trap "{ echo 'An error occurred at command “${BASH_COMMAND}”' }" ERR
+trap "{ echo 'An error occurred at command “${BASH_COMMAND}”'; }" ERR
 
-# Mount backup disk
-DISK_DIRECTORY="/dev"
+if [ ! "${1}" ]
+then
+	read -p "Target [disk/cloud]: " target
+else
+	target="${1}"
+fi
 
-lsblk
-read -p "Disk: ${DISK_DIRECTORY}/" disk
-if [ ! "${disk}" ]; then exit 1; fi
-MOUNT_DIRECTORY="/mnt/${disk}"
+if [ "${target}" == "disk" ]
+then
+	# Mount backup disk
+	DISK_DIRECTORY="/dev"
 
-mkdir --parents "${MOUNT_DIRECTORY}"
-mount "${DISK_DIRECTORY}/${disk}" "${MOUNT_DIRECTORY}"
+	lsblk
+	read -p "Disk: ${DISK_DIRECTORY}/" disk
+	if [ ! "${disk}" ]; then exit 1; fi
+	MOUNT_DIRECTORY="/mnt/${disk}"
+
+	mkdir --parents "${MOUNT_DIRECTORY}"
+	mount "${DISK_DIRECTORY}/${disk}" "${MOUNT_DIRECTORY}"
+elif [ "${target}" == "cloud" ]
+then
+	# Mount backup storage
+	MOUNT_DIRECTORY="/mnt/cloud"
+
+	mkdir --parents "${MOUNT_DIRECTORY}"
+	sshfs -o allow_root,uid=0,gid=0 -p 23 cloud:/home "${MOUNT_DIRECTORY}"
+else
+	echo "Unknown target ${target}"
+	exit 1
+fi
 
 # Create backup
 export BORG_REPO="${MOUNT_DIRECTORY}/borg"
 
 read -s -p "Passphrase: " passphrase
-if [ "${passphrase}" ]; then export BORG_PASSPHRASE=${passphrase}; fi
+if [ $passphrase ]
+then
+	export BORG_PASSPHRASE=$passphrase
+fi
 
-borg create                                             \
-    --verbose                                           \
-    --filter AMCE                                       \
-    --list                                              \
-    --stats                                             \
-    --compression lzma                                  \
-    --exclude-caches                                    \
-    --exclude '*/.cache/*'                              \
-    --exclude '*/tmp/*'                                 \
-    --exclude 'home/borg/*'                             \
-    --exclude 'home/factorio/temp'                      \
-    --exclude '*/_data/models/*'                        \
-    --exclude 'opt/immich/library/thumbs/*'             \
-    --exclude 'opt/immich/library/encoded-video/*'      \
-                                                        \
-    ::'{now}'                                           \
-    /etc                                                \
-    /home                                               \
-    /root                                               \
+borg create --verbose \
+    --filter AMCE \
+    --list \
+    --stats \
+    --compression lzma \
+    --exclude-caches \
+    --exclude '*/.cache/*' \
+    --exclude '*/cache/*' \
+    --exclude '*/tmp/*' \
+    ::'{now}' \
+    /etc \
+    /home \
+    /root \
     /opt
 
 # Prune repository
-borg prune                 \
-    --list                 \
-    --keep-daily   14      \
-    --keep-weekly   4      \
-    --keep-monthly 24      \
-    --keep-yearly   5
+borg prune \
+    --list \
+    --keep-daily 14 \
+    --keep-weekly 4 \
+    --keep-monthly 24 \
+    --keep-yearly 5
 
 # Compact repository
 borg compact
 
-# Unmount backup disk
-umount "${MOUNT_DIRECTORY}"
+if [ "${target}" == "disk" ]
+then
+	# Unmount backup disk
+	umount "${MOUNT_DIRECTORY}"
+elif [ "${target}" == "cloud" ]
+then
+	# Unmount backup storage
+	fusermount -u "${MOUNT_DIRECTORY}"
+fi
