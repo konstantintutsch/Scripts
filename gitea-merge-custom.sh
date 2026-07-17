6514677c1a17d987d5a2b945578ed8a61c1c664c
Konstantin Tutsch <mail@konstantintutsch.com>
2026-07-17 06:23:28 +0200

    fix: branch not updated, when already existing locally
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

--- gitea-merge-custom.sh
+++ gitea-merge-custom.sh
@@ -2,12 +2,16 @@
 
 set -e # exit if any command fails
 
+REMOTE="gitea"
+
 read "pr?PR: "
 
 read "target_branch?Target Branch: "
 
 read "source_branch?Source Branch: "
 
-git fetch gitea "${source_branch}"
+git fetch "${REMOTE}" "${source_branch}"
+git pull --ff-only "${REMOTE}" "${source_branch}"
+
 git checkout --quiet "${target_branch}"
-git merge --signoff -m "merge: ${source_branch} (#${pr})" "gitea/${source_branch}"
+git merge --signoff -m "merge: ${source_branch} (#${pr})" "${REMOTE}/${source_branch}"
