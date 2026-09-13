bf413818b903fc8501f521233a0fa20c6bdce4e4
Konstantin Tutsch <mail@konstantintutsch.com>
2026-09-13 07:35:49 +0200

    style: move sshfs port to variable
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

--- templates/backup.sh
+++ templates/backup.sh
@@ -11,6 +11,7 @@ LOGFILE="backup.log"
 
 CLOUD_SERVER="<server>"
 CLOUD_USER="<user>"
+CLOUD_PORT="23"
 CLOUD_DIRECTORY="/home"
 
 BACKUP_DIRECTORY="borg"
@@ -52,7 +53,7 @@ then
 	MOUNT_DIRECTORY="/mnt/cloud"
 
 	mkdir --parents "${MOUNT_DIRECTORY}"
-    sshfs -o allow_root,uid=0,gid=0 -p 23 "${CLOUD_USER}"@"${CLOUD_SERVER}":"${CLOUD_DIRECTORY}" "${MOUNT_DIRECTORY}"
+    sshfs -o allow_root,uid=0,gid=0 -p ${CLOUD_PORT} "${CLOUD_USER}"@"${CLOUD_SERVER}":"${CLOUD_DIRECTORY}" "${MOUNT_DIRECTORY}"
 else
 	echo "Unknown target ${target}"
 	exit 1
