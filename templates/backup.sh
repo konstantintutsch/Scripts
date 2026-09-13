8df8afac58925a03ec693e600b6da38a661e5026
Konstantin Tutsch <mail@konstantintutsch.com>
2026-09-13 07:34:53 +0200

    fix: pass arguments through to screen session
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

--- templates/backup.sh
+++ templates/backup.sh
@@ -24,7 +24,7 @@ fi
 # Run screen if not already inside
 if [ -z "$STY" ]
 then
-	exec screen -L -Logfile "$LOGFILE" -dmS backup /bin/bash "$0"
+	exec screen -L -Logfile "$LOGFILE" -dmS backup /bin/bash "$0" "$@"
 fi
 
 if [ ! "${1}" ]
